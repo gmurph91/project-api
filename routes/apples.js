5567d815bb7dc8bef838a00fccd6072e6e8bb931
greg <murphygreg6@gmail.com>
2020-03-02 18:56:19 -0700

    fixed file name

--- routes/apples.js
+++ routes/apples.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const codeAccess = require('../data_access_layer/apples')
+const codeAccess = require('../data_access_layer/appleaccess')
 
 router.get('/green', codeAccess.Green);
 router.get('/red', codeAccess.Red);
